c68a5481302369bcba0b4a260461eb5131970f38
ElliotArn <119504123+ElliotArn@users.noreply.github.com>
2023-09-04 01:32:26 +0200

    Update info.sh

--- serverThings/info.sh
+++ serverThings/info.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-"/mnt/e/sak/random program/makemkv/real/MakeMKV/makemkvcon64.exe" "info" "$1" --minlength=1800 | grep -c "was added"
+makemkvcon "info" "$1" --minlength=1800 | grep -c "was added"
