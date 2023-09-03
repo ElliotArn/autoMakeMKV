d0e69104b9a0b66ff91bd761dbf2bb1bfd318c8a
ElliotArn <119504123+ElliotArn@users.noreply.github.com>
2023-09-04 01:32:57 +0200

    Update mkvScript.sh

--- serverThings/mkvScript.sh
+++ serverThings/mkvScript.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-"/mnt/e/sak/random program/makemkv/real/MakeMKV/makemkvcon64.exe" "--minlength=1800" "mkv" "$1" "$2" "$3" > /dev/null
+makemkvcon "--minlength=1800" "mkv" "$1" "$2" "$3" > /dev/null
