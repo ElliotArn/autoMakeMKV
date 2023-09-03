86d9bc4cbf41d8dbf401934e83d53027232e8643
ElliotArn <119504123+ElliotArn@users.noreply.github.com>
2023-09-04 01:35:18 +0200

    Update send.sh

--- serverThings/send.sh
+++ serverThings/send.sh
@@ -1,18 +1,13 @@
+
 #!/bin/bash
-
-name="$(cmd.exe "/mnt/c/temp/getName.bat" | grep D: | awk '{print $4}')"
+name="$(blkid -s LABEL -o value)"
 echo -e "\n\n\n\n\n"
-
 read -p ""$name"   :" input
-
 if [ "$input" == "" ]; then
-	new="/root/serverThings/output/$name.mkv"
-else
-	new=/root/serverThings/output/"$input".mkv
+new="/home/thomas/Documents/autoMakeMKV/serverThings/output/$name.mkv"
+	else
+new=/home/thomas/Documents/autoMakeMKV/serverThings/output/"$input".mkv
 fi
-
-mv /root/serverThings/output/* "$new"
-
+mv /home/thomas/Documents/autoMakeMKV/serverThings/output/* "$new"
 scp "$new" "arn@192.168.1.14:/home/arn/data/movies"
-
 rm "$new"
