19311c6b92eeb775a8d9b7c0093d7b3dae508374
Elliot <elliot.arn@gmail.com>
2023-09-15 02:44:09 +0200

    hej

--- serverThings/do.py
+++ serverThings/do.py
@@ -4,25 +4,26 @@ import subprocess, threading, time, re
 
 outputdir="output"
 titles=[]
-hej="./hej.sh"
 infoScript="./info.sh"
 disk="disc:0"
 makemkvScript="./mkvScript.sh"
-killFake="./KillFake.sh"
+killFake="./killFake.sh"
 renameAndSend="./send.sh"
 
 
 def makeMkv(title):
-    subprocess.run([makemkvScript, disk, title, outputfile])
+    subprocess.run([makemkvScript, disk, title, outputdir])
 
 def kill():
     time.sleep(300)
-    entries = os.listdir(outputfile)
+    entries = os.listdir(outputdir)
     if not entries:
         subprocess.run([killFake, makemkvScript])
 
+os.system('rm /home/thomas/autoMakeMKV/serverThings/output/* 2> /dev/null')
 
-thing=subprocess.run([infoScript, disk],capture_output=True)
+thing = subprocess.run([infoScript, disk],capture_output=True)
+
 
 print(thing.stdout)
 
@@ -40,6 +41,7 @@ for a in range(int(titles)):
     t.join()  # Wait for t to finish
     t2.join()  # Wait for t2 to finish
     print("Both threads have finished")
+    print('\a')
     subprocess.run([renameAndSend])
 
 
--- serverThings/send.sh
+++ serverThings/send.sh
@@ -1,13 +1,12 @@
-
 #!/bin/bash
 name="$(blkid -s LABEL -o value)"
 echo -e "\n\n\n\n\n"
 read -p ""$name"   :" input
 if [ "$input" == "" ]; then
-new="/home/thomas/Documents/autoMakeMKV/serverThings/output/$name.mkv"
+new="/home/thomas/autoMakeMKV/serverThings/output/$name.mkv"
 	else
-new=/home/thomas/Documents/autoMakeMKV/serverThings/output/"$input".mkv
+new=/home/thomas/autoMakeMKV/serverThings/output/"$input".mkv
 fi
-mv /home/thomas/Documents/autoMakeMKV/serverThings/output/* "$new"
-scp "$new" "arn@192.168.1.14:/home/arn/data/movies"
+mv /home/thomas/autoMakeMKV/serverThings/output/* "$new"
+scp "$new" "arn@192.168.1.14:/home/arn/raid/movies"
 rm "$new"
